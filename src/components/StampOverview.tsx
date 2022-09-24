553dcf03bdfde53d1b7f996ca03ad06ab8897439
chase-manning <chase@manning.dev>
2022-09-24 21:49:24 +0100

    :rocket: add header to stamp overview

--- src/components/StampOverview.tsx
+++ src/components/StampOverview.tsx
@@ -1,11 +1,34 @@
 import styled from "styled-components";
 import StampPopup from "./StampPopup";
 
+import header from "../assets/stamps/header.svg";
+import Hexify from "./Hexify";
+
 const StyledStampOverview = styled.div`
   display: flex;
   height: 100%;
   width: 100%;
-  background: yellow;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: flex-start;
+  justify-content: space-between;
+`;
+
+const HeaderIcon = styled.img`
+  height: 5.7rem;
+`;
+
+const Button = styled.button`
+  height: 5.7rem;
+  margin: 0 1.5rem;
+  color: #9b8321;
+  font-weight: 500;
+  font-size: 3.5rem;
+  line-height: 50px;
 `;
 
 interface Props {
@@ -17,8 +40,15 @@ interface Props {
 const StampOverview = ({ show, close, setStamp }: Props) => {
   return (
     <StampPopup show={show} close={close}>
-      <StyledStampOverview onClick={() => setStamp("meow")}>
-        meow
+      <StyledStampOverview>
+        <Header>
+          <HeaderIcon src={header} alt="Stamp header icon" />
+          <Hexify yellow>
+            <Button onClick={() => alert("Not implemented yet")}>
+              REWARDS
+            </Button>
+          </Hexify>
+        </Header>
       </StyledStampOverview>
     </StampPopup>
   );
